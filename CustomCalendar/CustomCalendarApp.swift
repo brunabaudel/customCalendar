fb3033666ce4b5148e4701b2bf7f080117db1b82
Bruna Baudel <brubaudel@gmail.com>
2023-10-14 11:00:28 +0200

    Initial Commit

---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by Bruna Baudel on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
