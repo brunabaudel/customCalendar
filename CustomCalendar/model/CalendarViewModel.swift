fb4c1d50cff40d6587030f948faffa028564b930
Bruna Baudel <brubaudel@gmail.com>
2023-11-20 13:07:26 +0100

    add logo

--- CustomCalendar/model/CalendarViewModel.swift
+++ CustomCalendar/model/CalendarViewModel.swift
@@ -72,33 +72,3 @@ class CalendarViewModel: ObservableObject {
         calendar.year += checkYear() ? 0 : 1
     }
 }
-
-extension Date {
-    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
-        return calendar.component(component, from: self)
-    }
-    
-    func startWeekDay(_ month: Int? = nil, _ year: Int? = nil) -> Int {
-        var dateComponents = DateComponents()
-        dateComponents.year = year ?? get(.year)
-        dateComponents.month = month ?? get(.month)
-        dateComponents.day = 1
-        
-        if let d = Calendar.current.date(from: dateComponents),
-           let day = Calendar.current.dateComponents(in: .current, from: d).weekday
-        { return day }
-        return -1
-    }
-    
-    func daysInMonth(_ month: Int? = nil, _ year: Int? = nil) -> Int {
-        var dateComponents = DateComponents()
-        dateComponents.year = year ?? get(.year)
-        dateComponents.month = month ?? get(.month)
-        if
-            let d = Calendar.current.date(from: dateComponents),
-            let interval = Calendar.current.dateInterval(of: .month, for: d),
-            let days = Calendar.current.dateComponents([.day], from: interval.start, to: interval.end).day
-        { return days }
-        return -1
-    }
-}
